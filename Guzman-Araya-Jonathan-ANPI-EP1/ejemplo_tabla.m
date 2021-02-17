61295d21d4efcb1b2a1196903bd0871c7d58bbb6
Jonathana1196 <jonathana1196@gmail.com>
2021-02-17 01:21:40 -0600

    Semestre I 2021

--- Guzman-Araya-Jonathan-ANPI-EP1/ejemplo_tabla.m
+++
@@ -1,9 +0,0 @@
-clc; clear
-
-pkg load dataframe
-a1=5; a2=10; a3=15;
-C = {"Patient Name", "Volume", "Quality", "Owner"; 
-     "Joe", 200, a1, "MR"; 
-     "Dana", 186, a2, "MR"; 
-     "Cassidy", 197, a3, "SP"};
-dataframe (C)
