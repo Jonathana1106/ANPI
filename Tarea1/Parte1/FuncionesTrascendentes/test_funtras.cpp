bbab3572e3d0e0296a4b2930658152dd23c09738
Jonathana1196 <jonathana1196@gmail.com>
2021-03-27 03:34:30 -0600

    No me da la division :(

--- Tarea1/Parte1/FuncionesTrascendentes/test_funtras.cpp
+++ Tarea1/Parte1/FuncionesTrascendentes/test_funtras.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    double fac;
+    double fac, expoNUNO;
 
-    fac = factorial(4);
+    cout << "El factorial de 7 es: ";
+    fac = factorial(7);
+    cout << "\n";
+    //cout << "El calculo de 2^-1 es: ";
+    //expoNUNO = exponenteN1(2);
 
     return 0;
 }
