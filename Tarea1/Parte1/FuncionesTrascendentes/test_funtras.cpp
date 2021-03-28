e2226cf2e2dd8027893e6bc30b186efcf8569068
Jonathana1196 <jonathana1196@gmail.com>
2021-03-28 13:32:45 -0600

    Tarea1

--- Tarea1/Parte1/FuncionesTrascendentes/test_funtras.cpp
+++ Tarea1/Parte1/FuncionesTrascendentes/test_funtras.cpp
@@ -1,56 +1,72 @@
 #include <iostream>
 #include "funtras.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
 
-    double fac, expe, expoNUNO, seno, coseno, tangente, logan, logb, powert, senoh, cosenoh, raiz, raizx, aseno, atan;
+int main() {
+
+    double fac, expe, expoNUNO, seno, coseno, tangente, logan, logb, powert, senoh, cosenoh, tanoh, raiz, raizx, aseno, atan;
 
     fac = factorial(7);
     cout << "El factorial de 7 es: " << fac;
     cout << "\n";
-    expe = exp_t(2);
-    cout << "El resultado de e^2 es: " << expe;
+    expe = exp_t(11);
+    cout << "El resultado de e^11 es: " << expe;
     cout << "\n";
-    expoNUNO = varM1(2);
-    cout << "El calculo de 2^-1 es: "<< expoNUNO;
+    expoNUNO = varM1(144);
+    cout << "El calculo de 144^-1 es: " << expoNUNO;
     cout << "\n";
-    seno = sin_t(1);
-    cout << "El seno de 1 es: "<< seno;
+    seno = sin_t(2.33);
+    cout << "El seno de 2.33 es: " << seno;
     cout << "\n";
-    coseno = cos_t(1);
-    cout << "El coseno de 1 es: "<< coseno;
+    coseno = cos_t(3.78);
+    cout << "El coseno de 3.78 es: " << coseno;
     cout << "\n";
     tangente = tan_t(7);
-    cout << "La tangente de 7 es: "<< tangente;
+    cout << "La tangente de 7 es: " << tangente;
     cout << "\n";
-    logan = ln_t(2);
-    cout << "El logaritmo natural de 2 es: "<< logan;
+    logan = ln_t(45);
+    cout << "El logaritmo natural de 45 es: " << logan;
     cout << "\n";
-    logb = log_t(2, 5);
-    cout << "El logaritmo en base 5 de 2 es: "<< logb;
+    logb = log_t(33, 43);
+    cout << "El logaritmo en base 5 de 2 es: " << logb;
     cout << "\n";
-    powert = power_t(2, 2);
-    cout << "El resultado de 2^2 es: "<< powert;
+    powert = power_t(2, 11);
+    cout << "El resultado de 2^11 es: " << powert;
     cout << "\n";
-    senoh = sinh_t(1);
-    cout << "El senoh de 1 es: "<< senoh;
+    senoh = sinh_t(2.1);
+    cout << "El senoh de 2.1 es: " << senoh;
     cout << "\n";
-    cosenoh = cosh_t(0);
-    cout << "El cosenoh de 1 es: "<< cosenoh;
+    cosenoh = cosh_t(0.89);
+    cout << "El cosenoh de 0.89 es: " << cosenoh;
     cout << "\n";
-    raiz = sqrt_t(3);
-    cout << "La raiz cuadrada de 3 es: "<< raiz;
+    tanoh = tanh_t(0.22);
+    cout << "El tanoh de 0.22 es: " << tanoh;
     cout << "\n";
-    raizx = root_t(9, 3);
-    cout << "La raiz cubica de 9 es: "<< raizx;
+    raiz = sqrt_t(144);
+    cout << "La raiz cuadrada de 144 es: " << raiz;
     cout << "\n";
-    aseno = asin_t(2);
-    cout << "El aseno de 2 es: "<< aseno;
+    int a = 7;
+    raizx = root_t(45, a);
+    cout << "La raiz " << a << " de 45 es:" << raizx;
     cout << "\n";
-    atan = atan_t(2);
-    cout << "El atan de 2 es: "<< atan;
+    aseno = asin_t(0.33);
+    cout << "El aseno de 0.33 es: " << aseno;
+    cout << "\n";
+    atan = atan_t(0.67);
+
+    cout << "El atan de 0.67 es: " << atan;
+    cout << "\n";
+    cout << "Funcion para punto 1.1:";
     cout << "\n";
 
+    double resultado = root_t((sin_t(3 * varM1(7)) + ln_t(2)), 3) * varM1(sinh_t(sqrt_t(2))) + atan_t(varM1(exp_t(1)));
+    cout << "El resultado de la funcion para el punto 1.1 es: " << resultado;
+    cout << "\n";
+    double pi = pi_t();
+    cout << "pi es: " << pi;
+    cout << "\n";
+    double acos = acos_t(0.23);
+    cout << "El acos de 0.23 es: " << acos;
+    cout << "\n";
     return 0;
 }
