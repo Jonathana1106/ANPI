2d98cb459f66a8e14575b25482c1bc79ceac2a90
Jonathan G Araya <jonathana1196@gmail.com>
2021-04-29 03:52:40 -0600

    Metodos de C++
    Falta graficar

---
+++ MetodosENL/FalsaPosicion.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <ginac/ginac.h>
+
+using namespace std;
+using namespace GiNaC;
+
+/**
+ *
+ * @param funcion: Funcion a la que se le va a aplicar el metodo
+ * @param firstValue: primer valor inicial
+ * @param secondValue: segundo valor inicial
+ * @param TOL: tolerancia
+ * @param MAXIT: cantidad maxima de iteraciones
+ * @return
+ * @return
+ */
+ex *falsaPosicion(string funcion, ex x0, ex x1, ex MAXIT, ex TOL) {
+
+    symbol x("x");
+    symtab table;
+    table["x"] = x;
+    parser reader(table);
+    ex f = reader(funcion);
+    ex f0 = evalf(subs(f, x == x0));
+    ex f1 = evalf(subs(f, x == x1));
+    ex fxk;
+    ex xk;
+    ex error = TOL + 1;
+    xk = x1 - ((x1 - x0) / (f1 - f0)) * f1;
+
+    int iter = 1;
+    static ex resultado[2];
+
+    while (iter < MAXIT) {
+        fxk = evalf(subs(f, x == xk));
+        if (f0 * fxk < 0) {
+            xk = xk - ((xk - x0) / (fxk - evalf(subs(f, x == x0)))) * fxk;
+            x1 = xk;
+            error = abs(evalf(subs(f, x == xk)));
+
+            if(error < TOL) {
+                break;
+            }
+            else {
+                iter = iter + 1;
+            }
+        } else if (fxk * f1 < 0) {
+            xk = xk - ((xk - x0) / (fxk - evalf(subs(f, x == x1)))) * fxk;
+            x0 = xk;
+            error = abs(evalf(subs(f, x == xk)));
+
+            if(error < TOL) {
+                break;
+            }
+            else {
+                iter = iter + 1;
+            }
+        } else {
+            cout << "No es posible de resolver mediante Falsa Posicion" << endl;
+            return 0;
+        }
+    }
+    resultado[0] = evalf(xk);
+    resultado[1] = abs(evalf(subs(f, x == xk)));
+    return resultado;
+}
+
+int main() {
+    ex *testFP;
+    testFP = falsaPosicion("cos(x)-x", 1 / 2, Pi / 4, 100, 0.0001);
+    cout << "Aproximacion: " << *testFP << endl;
+    cout << "Error: " << *(testFP + 1) << endl;
+    return 0;
+}
---
+++ MetodosENL/Secante.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <ginac/ginac.h>
+
+using namespace std;
+using namespace GiNaC;
+
+/**
+ *
+ * @param funcion: Funcion a evaluar en el metodo
+ * @param x0: primer valor inicial
+ * @param x1: segundo valor inicial
+ * @param MAXIT: cantidad maxima de iteraciones
+ * @param TOL: tolerancia del resultado
+ * @return
+ */
+ex *secante(string funcion, ex x0, ex x1, ex MAXIT, ex TOL) {
+    symbol x;
+    symtab table;
+    table["x"] = x;
+    parser reader(table);
+    ex f = reader(funcion);
+    ex xk = x1;
+    ex xkm1 = x0;
+    ex xk1;
+    int iter = 0;
+    ex err = TOL + 1;
+    static ex resultado[2];
+
+    while (iter < MAXIT) {
+        xk1 = xk -
+              ((((xk - xkm1)) / ((evalf(subs(f, x == xk))))) - evalf(subs(f, x == xkm1))) * (evalf(subs(f, x == xk)));
+        xkm1 = xk;
+        xk = xk1;
+        err = abs(evalf(subs(f, x == xk)));
+
+        if (err < TOL) {
+            break;
+        } else {
+            iter = iter + 1;
+        }
+    }
+    resultado[0] = xk;
+    resultado[1] = abs((evalf(subs(f, x == xk))));
+    return resultado;
+}
+
+int main(void) {
+    ex *testS;
+    testS = secante("exp(-pow(x, 2)) - x", 0, 1, 100, 0.001);
+    cout << "Aproximacion: " << *testS << endl;
+    cout << "Error: " << *(testS + 1) << endl;
+    return 0;
+}
